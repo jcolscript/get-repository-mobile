884ce274c631f4517a5fabdb3bac362c18439b86
José Carlos <jcolscript@gmail.com>
2020-04-23 00:45:22 -0300

    construindo esquema de navegacao

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/reactotron';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-});
-
-console.tron.log('Hello World!');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.body}>
-      <Text style={styles.sectionTitle}>Hello World</Text>
-    </View>
-  );
+  return <Routes />;
 }
---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerBackTitleVisible: false,
+          headerStyle: {
+            backgroundColor: '#5063F0',
+          },
+          headerTintColor: '#fff',
+        }}
+      >
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ title: 'Usuários' }}
+        />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
