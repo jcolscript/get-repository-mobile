1f2e1663193cb98f10ae9bafb4187c5af935a13b
José Carlos <jcolscript@gmail.com>
2020-04-24 22:52:35 -0300

    configurando o stuled componentes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import { Container } from './styles';
 
 const Main = () => {
-  return <View />;
+  return <Container />;
 };
 
 export default Main;
