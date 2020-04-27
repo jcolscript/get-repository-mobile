067ef602b191ac83d307f69f807fecb817ca121f
José Carlos <jcolscript@gmail.com>
2020-04-26 23:39:13 -0300

    implementando a chamada da api no componente Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Keyboard } from 'react-native';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-const Main = () => {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#ffffff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
-};
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
 
-export default Main;
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#ffffff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
