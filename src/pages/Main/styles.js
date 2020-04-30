9739ec990ac21447191503579f5da6a3a1e5bd6f
José Carlos <jcolscript@gmail.com>
2020-04-30 01:40:13 -0300

    exibindo repos do user em nova screem

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,7 +4,7 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  padding: 30px 30px 0px 30px;
   background: #fff;
 `;
 
