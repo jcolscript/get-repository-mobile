0f17f67aedaf03e518f15b25141e46ba53cb7d7b
José Carlos <jcolscript@gmail.com>
2020-04-30 01:40:24 -0300

    listando repositorios

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,71 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const User = ({ route }) => {
-  console.tron.log(route.params.user);
-  return <View />;
-};
+import api from '../../services/api';
 
-export default User;
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Repos,
+  Repo,
+  RepoName,
+  Info,
+  HeaderRepo,
+} from './styles';
+
+export default class User extends Component {
+  static propTypes = {
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.shape().isRequired,
+      }).isRequired,
+    }).isRequired,
+  };
+
+  state = {
+    repos: [],
+  };
+
+  async componentDidMount() {
+    const { route } = this.props;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/repos`);
+
+    this.setState({ repos: response.data });
+  }
+
+  render() {
+    const { repos } = this.state;
+    const { route } = this.props;
+    const { user } = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          {user.bio && <Bio>{user.bio}</Bio>}
+        </Header>
+
+        <Repos
+          data={repos}
+          keyExtractor={(repo) => String(repo.id)}
+          renderItem={({ item }) => (
+            <Repo>
+              <HeaderRepo>
+                <Icon name="collections-bookmark" size={20} color="#5063f0" />
+                <RepoName>{item.name}</RepoName>
+              </HeaderRepo>
+              <Info>{item.description}</Info>
+            </Repo>
+          )}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/pages/User/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px 30px 0px 30px;
+  background: #fff;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 10px;
+  text-align: center;
+`;
+export const Bio = styled.Text`
+  font-size: 14px;
+  line-height: 18px;
+  color: #333;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const Repos = styled(FlatList).attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 15px;
+`;
+
+export const Repo = styled.View`
+  flex-direction: column;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+`;
+
+export const HeaderRepo = styled.View`
+  flex-direction: row;
+  margin-bottom: 5px;
+  align-items: center;
+`;
+
+export const RepoName = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+  color: #5063f0;
+  margin-left: 4px;
+`;
+
+export const Info = styled.Text`
+  font-size: 14px;
+  line-height: 18px;
+  color: #333;
+`;
